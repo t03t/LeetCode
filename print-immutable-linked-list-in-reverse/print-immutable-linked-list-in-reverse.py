c82686bf8231afefe45d8468c7819b1fc47bb3e0
S. Khadija T <52125592+t03t@users.noreply.github.com>
2021-11-10 01:27:00 -0500

    Time: 32 ms (91.34%), Space: 15.3 MB (9.27%) - LeetHub

---
+++ print-immutable-linked-list-in-reverse/print-immutable-linked-list-in-reverse.py
@@ -0,0 +1,23 @@
+# """
+# This is the ImmutableListNode's API interface.
+# You should not implement it, or speculate about its implementation.
+# """
+# class ImmutableListNode:
+#     def printValue(self) -> None: # print the value of this node.
+#     def getNext(self) -> 'ImmutableListNode': # return the next node.
+
+class Solution:
+    def printLinkedListInReverse(self, head: 'ImmutableListNode') -> None:
+        
+        def helper(node):
+            if node.getNext():
+                helper(node.getNext())
+            node.printValue()
+            return node 
+        
+        if not head.getNext():
+            head.printValue()
+            return
+        head = helper(head)
+        
+        
