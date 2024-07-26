490ef808df5d8ae1bc0b0ff3e2ca1c9033458f73
Tirmazi <52125592+t03t@users.noreply.github.com>
2024-07-26 15:27:48 -0700

    Time: 128 ms (5.99%), Space: 54.4 MB (5.91%) - LeetHub

---
+++ 2741-function-composition/2741-function-composition.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    return function(x) {
+        result = x;
+        for (let func of functions.reverse() ) {
+            console.log(func(x));
+            result = func(result);
+        }
+        return result;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
