e654686a8c8c4be1ee2ef12883aeb4bc1c7d21a7
Tirmazi <52125592+t03t@users.noreply.github.com>
2024-07-27 13:09:59 -0700

    Time: 58 ms (51.32%), Space: 51.4 MB (35.4%) - LeetHub

--- 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.ts
+++ 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.ts
@@ -1,11 +1,7 @@
 type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
 
 function argumentsLength(...args: JSONValue[]): number {
-    let count = 0;
-    for (const arg of args) {
-        count += 1;
-    }
-    return count;
+    return args.length
 };
 
 /**
