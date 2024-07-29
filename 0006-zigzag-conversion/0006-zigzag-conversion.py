096223fc92aa88484871a801fc2a78807f1a4c32
Tirmazi <52125592+t03t@users.noreply.github.com>
2024-07-28 20:42:01 -0700

    Time: 43 ms (90.03%), Space: 16.6 MB (49.2%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,22 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows==1:
+            return s
+        rows = [""] * numRows
+
+        res = ""
+        graph = defaultdict(list)
+        s = list(s)
+
+        i = 0
+        inc = 1
+        for elem in s:
+            rows[i] += elem
+            if i == 0:
+                inc = 1
+            elif i == numRows - 1:
+                inc = -1
+            i += inc
+        
+
+        return "".join(rows)
