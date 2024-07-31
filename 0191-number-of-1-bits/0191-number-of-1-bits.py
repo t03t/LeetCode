cb2fedc08942cf90760b731a577c7546b825534e
Tirmazi <52125592+t03t@users.noreply.github.com>
2024-07-30 21:20:57 -0700

    Time: 37 ms (47.14%), Space: 16.5 MB (63.01%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -3,13 +3,8 @@ class Solution:
         two_power = int(math.log(n, 2))
         hamming = 0
         while (n > 0 and two_power >= 0):
-            print("N: ", n)
-            print("power: ", 2**two_power)
             if (n >= 2**two_power):
                 hamming += 1
                 n -= 2**two_power
             two_power -= 1
-            print("hamming: ", hamming)
         return hamming
-        if n == 0:
-            return 0
