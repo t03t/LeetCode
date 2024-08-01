7998edd3da4a3c682b001c3de22f35fbf7891c61
Tirmazi <52125592+t03t@users.noreply.github.com>
2024-08-01 09:56:55 -0700

    Time: 68 ms (38.09%), Space: 51.9 MB (66.87%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -5,12 +5,7 @@
 var isPalindrome = function(s) {
     let word = (s.replaceAll(/[^0-9a-z]/gi, '').replaceAll(" ", "").toLowerCase());
     let length = word.length;
-    console.log(length);
-    console.log(word.slice(0, length));
-    console.log(word.slice(0, length/2));
-    console.log(word.slice(length/2 +1, length).split("").reverse().join(""));
     if (length%2 === 0) {
-        console.log("even")
         return word.slice(0, length/2) == word.slice(length/2, length).split("").reverse().join("");
     }
     return word.slice(0, length/2) == word.slice(length/2 + 1, length).split("").reverse().join("")
