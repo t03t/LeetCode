796ab0126519dde332e58d46c8d2081d9d180940
Tirmazi <52125592+t03t@users.noreply.github.com>
2024-08-16 18:38:57 -0700

    Time: 41 ms (29.11%), Space: 16.4 MB (93.22%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,16 +1,7 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        org = n
-        n = abs(n)
-        cache = {}
-        while n:
-            if n in cache:
-                return cache[n] == 1
-            sum_of_squares = 0
-            for dig in list(str(n)):
-                sum_of_squares += int(dig)**2
-            cache[n] = sum_of_squares
-            if sum_of_squares == 1:
-                cache[org] = True
-                return True
-            n = sum_of_squares
+        cache = set()
+        while n!=1 and n not in cache:
+            cache.add(n)
+            n = sum(int(dig) ** 2 for dig in str(n))
+        return n==1
