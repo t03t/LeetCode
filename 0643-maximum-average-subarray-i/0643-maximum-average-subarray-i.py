4e35acd9b732947f5d1b8868ffcd27b9b62316de
Tirmazi <52125592+t03t@users.noreply.github.com>
2024-09-01 13:21:38 -0700

    Time: 863 ms (86.12%), Space: 28.5 MB (30.29%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -6,7 +6,6 @@ class Solution:
         sum_ = sum(nums[l:r+1])
         max_sum = sum_
         while r < len(nums) -  1:
-            print(l, r, sum_, max_sum)
             if sum_ > max_sum :
                 max_sum = sum_
             sum_ -= nums[l]
