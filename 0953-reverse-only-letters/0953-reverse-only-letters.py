e3f627d226e17cce049956f1f9fd5faf91a93cec
Tirmazi <52125592+t03t@users.noreply.github.com>
2024-09-02 22:17:17 -0700

    Time: 38 ms (35.89%), Space: 16.6 MB (7.22%) - LeetHub

--- 0953-reverse-only-letters/0953-reverse-only-letters.py
+++ 0953-reverse-only-letters/0953-reverse-only-letters.py
@@ -3,12 +3,12 @@ class Solution:
         s = [*s]
         l, r = 0, len(s) - 1
         while l < r:
-            if not s[l].isalpha():
-                l += 1
-            elif not s[r].isalpha():
-                r -= 1
-            else:
+            if s[l].isalpha() and s[r].isalpha():
                 s[l], s[r] = s[r], s[l]
                 l += 1
+                r -= 1
+            while l < r and not s[l].isalpha():
+                l += 1
+            while l < r and not s[r].isalpha():
                 r -= 1
         return ''.join(s)
