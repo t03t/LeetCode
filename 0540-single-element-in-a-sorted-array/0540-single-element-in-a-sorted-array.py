fddffc0bfed701b1fa7d03fb4e7f09a5f4096461
Tirmazi <52125592+t03t@users.noreply.github.com>
2024-10-15 17:31:36 -0700

    Time: 135 ms (78.78%), Space: 26.6 MB (65.15%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -3,8 +3,10 @@ class Solution:
         l, r = 0, len(nums) - 1
         while l < r:
             m = (l + r) // 2
-            if nums[m] == nums[m ^ 1]:
-                l = m + 1
+            if m % 2 == 1:
+                m -= 1
+            if nums[m] == nums[m + 1]:
+                l = m + 2
             else:
                 r = m
         return nums[l]
