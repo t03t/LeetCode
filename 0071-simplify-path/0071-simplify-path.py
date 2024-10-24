8c8f7147b6f2e01be9d5e6bd996d78d959c29fd5
Tirmazi <52125592+t03t@users.noreply.github.com>
2024-10-23 23:02:07 -0700

    Time: 0 ms (100%), Space: 16.5 MB (73.11%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,17 +1,13 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
         stack = []
-        print(path.split('/'))
         for fileName in path.split('/'):
             if not fileName:
                 continue
-            print(fileName)
-            ##
             if fileName == ".." and stack:
                 stack.pop()
             elif fileName == ".." or fileName == ".":
                 continue
             else:
                 stack.append(fileName)
-        print(stack)
         return '/'+'/'.join(stack)
