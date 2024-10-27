97790c7ef8f3737a46ca63865cabafcfe762fd72
Tirmazi <52125592+t03t@users.noreply.github.com>
2024-10-27 11:44:49 -0700

    Time: 98 ms (67.58%), Space: 26.1 MB (96.33%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -2,10 +2,9 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         res = [0] * len(temperatures)
         mono_stack = []
-
-        for day, tmp in enumerate(temperatures):
-            while mono_stack and temperatures[mono_stack[-1]] < tmp:
+        for i, temp in enumerate(temperatures):
+            while mono_stack and temperatures[mono_stack[-1]] < temperatures[i]:
                 j = mono_stack.pop()
-                res[j] = day - j
-            mono_stack.append(day)
+                res[j] = i - j
+            mono_stack.append(i)
         return res
