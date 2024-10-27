25dce6911971959c567e9d2bfa72a2fb61e54801
Tirmazi <52125592+t03t@users.noreply.github.com>
2024-10-27 12:07:44 -0700

    Time: 277 ms (7.53%), Space: 21.6 MB (43.41%) - LeetHub

---
+++ 0937-online-stock-span/0937-online-stock-span.py
@@ -0,0 +1,19 @@
+class StockSpanner:
+    # Span = maximum number of consecutive days in which stock_pice was less than or equal to the price of that day
+
+    def __init__(self):
+        self.mono_stack = []
+        
+    def next(self, price: int) -> int:
+        print("mono_stack: ", self.mono_stack, " price: ", price)
+        res = 1
+        while self.mono_stack and price >= self.mono_stack[-1][0]:
+            res += self.mono_stack[-1][1]
+            self.mono_stack.pop()
+        self.mono_stack.append([price, res])
+        return res
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
