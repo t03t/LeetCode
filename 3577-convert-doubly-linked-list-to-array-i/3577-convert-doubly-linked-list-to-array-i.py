eae3835b54d8ce44322ec96ee10fa5b7d2148427
Coffee <52125592+t03t@users.noreply.github.com>
2024-12-21 16:55:50 -0800

    Time: 50 ms (15.24%), Space: 17.9 MB (9.47%) - LeetHub

---
+++ 3577-convert-doubly-linked-list-to-array-i/3577-convert-doubly-linked-list-to-array-i.py
@@ -0,0 +1,15 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev=None, next=None):
+        self.val = val
+        self.prev = prev
+        self.next = next
+"""
+class Solution:
+    def toArray(self, root: 'Optional[Node]') -> List[int]:
+        res = []
+        while root:
+            res.append(root.val)
+            root = root.next
+        return res
