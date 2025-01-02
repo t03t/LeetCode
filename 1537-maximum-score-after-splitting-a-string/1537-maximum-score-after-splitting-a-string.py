0f1b168606fbfe479290dd28eed727c1ce2227aa
Coffee <52125592+t03t@users.noreply.github.com>
2025-01-02 08:53:24 -0800

    Time: 3 ms (58.17%), Space: 17.7 MB (23.15%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
@@ -6,7 +6,6 @@ class Solution:
         for i in range(len(s) - 1, -1, -1):
             prefix_sum[i] = int(s[i]) + prev
             prev = prefix_sum[i]
-        print(prefix_sum)
         l, r = 0, 0
         res = 0
         for i in range(len(s) - 1):
