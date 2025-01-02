f70b63c7727fb70d5315b40f3c71356f85f780ef
Coffee <52125592+t03t@users.noreply.github.com>
2025-01-01 19:53:42 -0800

    Time: 0 ms (100%), Space: 17.7 MB (14.99%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        dict = {'2':"abc", '3':"def", '4':"ghi", '5':"jkl", '6':"mno", '7': "pqrs", 
+        '8':"tuv", '9':"wxyz"}
+        cmb = [''] if digits else []
+        for d in digits:
+            cmb = [p + q for p in cmb for q in dict[d]]
+        return cmb
